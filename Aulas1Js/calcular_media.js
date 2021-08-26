22a4d5a2953e747acacf40de454e72748d7cdfc8
Ramon Diego <ramondiego857@gmail.com>
2021-08-26 12:30:12 -0300

    Aula de lógica e Js
    Enfoque em criação de funções , variáveis e interação de formulário

---
+++ Aulas1Js/calcular_media.js
@@ -0,0 +1,15 @@
+function calcular_media(nota1, nota2, nota3, nota4) {
+    // Declaração de variáveis
+    var media;
+
+    // Entrada de dados
+    
+
+
+    // Processamento de Dados
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    // Saída de Dados
+    console.log("Média é igual a "+media);
+    alert("Média é igual a "+media);
+}
