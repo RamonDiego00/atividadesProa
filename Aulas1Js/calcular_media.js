b59cb8e97660e91951dbc6e81607e4708bcb5d25
Ramon Diego <ramondiego857@gmail.com>
2021-08-31 19:28:24 -0300

    Utilizando estrutura de lógica
    Usamos if, else e else if na criação iterativa dos gifs e as respostas das médias

--- Aulas1Js/calcular_media.js
+++ Aulas1Js/calcular_media.js
@@ -16,11 +16,23 @@ function calcular_media() {
     // Saída de Dados
     $(".form-notas").addClass("d-none")
     $(".show-result").removeClass("d-none")
-    $(".result-value").html(media);
+    $(".result-value").html(media)
+
+    if(media >= 7) {
+        $(".description-result").html(`Parabéns, a sua nota é ${media}`)
+    }
+    else if(media < 7 && media > 4 ) {
+        $(".description-result").html(`Você precisa melhorar`)
+        $(".img-result").attr("src", "https://www.cursosapientia.com.br/admimg/siteBlog/cacd-diplomata-itamaraty-diplomacia-o-que-voce-precisa-saber-antes-de-comecar-a-estudar-para-o-cacd.png")
+    }
+    else {
+        $(".description-result").html(`Não sei o que vc fez mas esudar é que não foi,sua nota final é ${media}`)
+        $(".img-result").attr("src", "https://giphy.com/embed/u2wg2uXJbHzkXkPphr/video")
+    }
+     
 
     // console.log("Média é igual a "+media);
-    // alert("Média é igual a "+media);
-
+    // alert("Média é igual a "+media); 
 
     return false
 }
