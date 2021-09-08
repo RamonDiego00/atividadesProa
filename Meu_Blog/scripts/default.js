e7861c5f1038f5cc282c9d590c74c2232addc1b1
Ramon Diego <ramondiego857@gmail.com>
2021-09-07 23:26:08 -0300

    Arrumando alguns detalhes
    Tentando na real

---
+++ Meu_Blog/scripts/default.js
@@ -0,0 +1,17 @@
+$("body").hide();
+$("body").click(function(){           
+    //Sem parâmetros: o efeito é executado em 400ms
+    $("#minhaDiv").fadeIn();
+    //Parâmetro com a duração em ms do efeito
+    $("#minhaDiv").fadeIn(1000);
+    //Parâmetro slow: o efeito é executado em 600ms
+    $("#minhaDiv").fadeIn("slow");
+    //Parâmetro fast: o efeito é executado em 200ms
+    $("#minhaDiv").fadeIn("fast");
+    //Informada duração e função de callback
+    $("#minhaDiv").fadeIn("fast",
+        function(){
+            alert("fade concluido");
+        }
+    );
+});
