b4f27b338744b420c9122648d124c2ea333d2639
Ramon Diego <ramondiego857@gmail.com>
2021-09-09 22:37:24 -0300

    Página do cv
    Só fizemos o cabeçalho do cv

--- Meu_Blog/scripts/default.js
+++ Meu_Blog/scripts/default.js
@@ -1,17 +1,20 @@
-$("body").hide();
-$("body").click(function(){           
-    //Sem parâmetros: o efeito é executado em 400ms
-    $("#minhaDiv").fadeIn();
-    //Parâmetro com a duração em ms do efeito
-    $("#minhaDiv").fadeIn(1000);
-    //Parâmetro slow: o efeito é executado em 600ms
-    $("#minhaDiv").fadeIn("slow");
-    //Parâmetro fast: o efeito é executado em 200ms
-    $("#minhaDiv").fadeIn("fast");
-    //Informada duração e função de callback
-    $("#minhaDiv").fadeIn("fast",
-        function(){
-            alert("fade concluido");
-        }
-    );
-});
+// $("body").hide();
+// $("body").click(function(){           
+//     //Sem parâmetros: o efeito é executado em 400ms
+//     $("#minhaDiv").fadeIn();
+//     //Parâmetro com a duração em ms do efeito
+//     $("#minhaDiv").fadeIn(1000);
+//     //Parâmetro slow: o efeito é executado em 600ms
+//     $("#minhaDiv").fadeIn("slow");
+//     //Parâmetro fast: o efeito é executado em 200ms
+//     $("#minhaDiv").fadeIn("fast");
+//     //Informada duração e função de callback
+//     $("#minhaDiv").fadeIn("fast",
+//         function(){
+//             alert("fade concluido");
+//         }
+//     );
+// });
+
+var images = document.querySelectorAll('img');
+new simpleParallax(images);
