91b50cbde93c4d24be18a8e3e8a9d8669bd3599a
Ramon Diego <ramondiego857@gmail.com>
2021-09-14 12:55:01 -0300

    SiteDoGrupo4
    este é um site que desenvolvemos durante  aula, onde executamos em conjunto

---
+++ SiteGrupo4/scripts/index.js
@@ -0,0 +1,21 @@
+
+function tema() {
+        
+        $(".cabeca").toggleClass("bg-light");
+        $(".cabeca").toggleClass("navbar-light");
+        $(".cabeca").toggleClass("bg-dark");
+        $(".cabeca").toggleClass("navbar-dark"); 
+        $(".pe").toggleClass("bg-secondary");
+        $(".pe").toggleClass("text-light");
+        $(".pe").toggleClass("bg-dark");
+        $("body").toggleClass("bg-light");
+        $("body").toggleClass("bg-dark");
+        $(".inici").toggleClass("bg-secondary");
+        $(".inici").toggleClass("bg-light");
+        $(".cont").toggleClass("text-white");
+        $(".cont").toggleClass("text-dark");
+        $(".carro").toggleClass("text-dark");
+        $(".carro").toggleClass("text-white");
+        
+        
+}
